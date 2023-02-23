d5fd41010796ae34fe92784c2c7e05cc01f33781
atwine <twinmugume@gmail.com>
2023-02-23 14:06:04 +0200

    update installation

--- k3s Installation.sh
+++ k3s Installation.sh
@@ -5,8 +5,7 @@
 
 #k3s installation on my server
 #in the docs there are many options that one can use to install k3s
-# curl -sfL https://get.k3s.io | sh -s - --node-ip 137.63.194.17 --advertise-address 137.63.194.17 --advertise-port 34801 --https-listen-port 34801 --service-node-port-range 30000-30100 --node-name master
-curl -sfL https://get.k3s.io | sh -s - --node-ip 102.34.160.121 --advertise-address 102.34.160.121 --service-node-port-range 30000-30100 --node-name master
+curl -sfL https://get.k3s.io | sh -s - --node-ip 137.63.194.17 --advertise-address 137.63.194.17 --advertise-port 34801 --https-listen-port 34801 --service-node-port-range 30000-30100 --node-name master
 
 #uninstall master
 #this will help clean up all the information that k3s has installed on the master node
@@ -26,3 +25,14 @@ curl -sfL https://get.k3s.io | K3S_NODE_NAME=k3s-worker-02 K3S_URL=https://137.6
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 kubectl get pods --all-namespaces
 helm ls --all-namespaces
+
+Date: 2023-Feb-23
+#sometimes there is a permission issue with k3s
+#for that use the link below to solve the issue.
+# http://vcloud-lab.com/entries/devops/rancher-k3s-yaml-permission-denied-when-using-kubectl-kubernetes
+
+#see permissions with this command
+ls -lsa /etc/rancher/k3s/k3s.yaml
+
+#change to admin access with this command
+sudo chown admin:admin /etc/rancher/k3s/k3s.yaml
